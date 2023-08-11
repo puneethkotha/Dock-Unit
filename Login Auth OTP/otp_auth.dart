1ba7b16bf64814831f7dcecf96b4be1db00aa943
Puneeth <85823685+puneethkotha@users.noreply.github.com>
2023-08-11 18:24:41 +0530

    Create otp_auth.dart

---
+++ Login Auth OTP/otp_auth.dart
@@ -0,0 +1,291 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'opt_auth_model.dart';
+export 'opt_auth_model.dart';
+
+class OptAuthWidget extends StatefulWidget {
+  const OptAuthWidget({Key? key}) : super(key: key);
+
+  @override
+  _OptAuthWidgetState createState() => _OptAuthWidgetState();
+}
+
+class _OptAuthWidgetState extends State<OptAuthWidget>
+    with TickerProviderStateMixin {
+  late OptAuthModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final animationsMap = {
+    'imageOnPageLoadAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onPageLoad,
+      effects: [
+        MoveEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 330.ms,
+          begin: Offset(0, -48),
+          end: Offset(0, 0),
+        ),
+      ],
+    ),
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => OptAuthModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 389,
+                    height: 847,
+                    decoration: BoxDecoration(
+                      color: FlutterFlowTheme.of(context).secondaryBackground,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: Image.asset(
+                          'assets/images/Untitled_design_(2).png',
+                        ).image,
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(24, 150, 24, 140),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.asset(
+                                      'assets/images/Black_&_White_Minimalist_Business_Logo_(2).png',
+                                      width: 130,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    24, 10, 24, 0),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Align(
+                                            alignment:
+                                                AlignmentDirectional(0, 0),
+                                            child: Padding(
+                                              padding: EdgeInsetsDirectional
+                                                  .fromSTEB(0, 10, 0, 0),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(8),
+                                                child: Image.asset(
+                                                  'assets/images/icons8-lock-64.png',
+                                                  width: 60,
+                                                  height: 80,
+                                                  fit: BoxFit.contain,
+                                                ),
+                                              ).animateOnPageLoad(animationsMap[
+                                                  'imageOnPageLoadAnimation']!),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Column(
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: [
+                                          Align(
+                                            alignment:
+                                                AlignmentDirectional(0, 0),
+                                            child: Padding(
+                                              padding: EdgeInsetsDirectional
+                                                  .fromSTEB(0, 20, 0, 0),
+                                              child: Text(
+                                                'You will get a OTP via SMS',
+                                                style:
+                                                    FlutterFlowTheme.of(context)
+                                                        .labelMedium,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding:
+                                                EdgeInsetsDirectional.fromSTEB(
+                                                    0, 20, 0, 0),
+                                            child: PinCodeTextField(
+                                              autoDisposeControllers: false,
+                                              appContext: context,
+                                              length: 6,
+                                              textStyle:
+                                                  FlutterFlowTheme.of(context)
+                                                      .bodyLarge,
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              enableActiveFill: false,
+                                              autoFocus: true,
+                                              enablePinAutofill: false,
+                                              errorTextSpace: 16,
+                                              showCursor: true,
+                                              cursorColor: Color(0xFF670E67),
+                                              obscureText: false,
+                                              hintCharacter: '*',
+                                              pinTheme: PinTheme(
+                                                fieldHeight: 44,
+                                                fieldWidth: 44,
+                                                borderWidth: 2,
+                                                borderRadius:
+                                                    BorderRadius.circular(15),
+                                                shape: PinCodeFieldShape.box,
+                                                activeColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .primaryText,
+                                                inactiveColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .alternate,
+                                                selectedColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .primary,
+                                                activeFillColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .primaryText,
+                                                inactiveFillColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .alternate,
+                                                selectedFillColor:
+                                                    FlutterFlowTheme.of(context)
+                                                        .primary,
+                                              ),
+                                              controller: _model.otp,
+                                              onChanged: (_) {},
+                                              autovalidateMode: AutovalidateMode
+                                                  .onUserInteraction,
+                                              validator: _model.otpValidator
+                                                  .asValidator(context),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            0, 20, 0, 0),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            FFButtonWidget(
+                                              onPressed: () async {},
+                                              text: 'Verify',
+                                              options: FFButtonOptions(
+                                                width: 120,
+                                                height: 40,
+                                                padding: EdgeInsetsDirectional
+                                                    .fromSTEB(0, 0, 0, 0),
+                                                iconPadding:
+                                                    EdgeInsetsDirectional
+                                                        .fromSTEB(0, 0, 0, 0),
+                                                color: Color(0xFF3D083D),
+                                                textStyle:
+                                                    FlutterFlowTheme.of(context)
+                                                        .labelSmall
+                                                        .override(
+                                                          fontFamily: 'Lexend',
+                                                          color:
+                                                              Color(0xFFF3F5F7),
+                                                          fontSize: 16,
+                                                        ),
+                                                elevation: 3,
+                                                borderSide: BorderSide(
+                                                  color: Colors.transparent,
+                                                  width: 1,
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 50, 0, 0),
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () async {
+                              context.pushNamed('HomePage');
+                            },
+                            child: Icon(
+                              Icons.arrow_circle_left_rounded,
+                              color: Color(0xFF670E67),
+                              size: 32,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
